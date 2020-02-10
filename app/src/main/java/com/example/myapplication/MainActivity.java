e53f5ad55ddbdb2f4ff593ca9e4e3c457befab9c
AlecArchambault <aarchambault@wisc.edu>
2020-02-10 15:02:49 -0600

    Start

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,4 +10,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void clickFunction(View view){
+        Log.i( "Info", "Button pushed");
+        EditText myTextField = (EditText) findViewById(R.id.editText);
+        Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
+        String str = myTextField.getText().toString();
+        goToActivity2(str);
+    }
+
+    public void goToActivity2(String s){
+        Intent intent = new Intent(this, Main2Activity.class);
+        intent.putExtra("message", s);
+        startActivity(intent);
+    }
 }
